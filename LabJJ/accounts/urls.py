66a2f95c72d778fd48f68e10e3e321dbf9a6f249
Felipe Barcellos <f_barcellos@terra.com.br>
2025-11-26 22:28:01 -0300

    adding backend for accounts with login, logout, signals, passwordReset

--- LabJJ/accounts/urls.py
+++ LabJJ/accounts/urls.py
@@ -5,5 +5,7 @@ from django.urls import include
 app_name = 'accounts'
 
 urlpatterns = [
-    path('token-auth/', views.CustomAuthToken.as_view(), name='token-auth')
+    path('token-auth/', views.CustomAuthToken.as_view(), name='token-auth'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('password_reset/',include('django_rest_passwordreset.urls',namespace='password_reset')),
 ]
